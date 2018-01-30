075de732ced23eee24d230a0a35a6394e69dc6a9
GrobbierElder <GrobbierElder@10.30.14.202>
2018-01-29 19:15:37 -0600

    Constructores y ventana Jframe

---
+++ poo-eiam/src/constructores/Alumno.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author GrobbierElder
+ */
+public class Alumno {
+    
+    private String nombre;
+    private int edad;
+    private boolean aprobado;
+
+    public Alumno(int x){
+        
+    }
+    /**
+     * @return the nombre
+     */
+    public String getNombre(){
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isAprobado() {
+        return aprobado;
+    }
+
+    public void setAprobado(boolean aprobado) {
+        this.aprobado = aprobado;
+    }
+    
+    
+}
---
+++ poo-eiam/src/constructores/ProbarConstructores.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author GrobbierElder
+ */
+public class ProbarConstructores {
+    
+    public static void main(String[] args) {
+        /*Alumno a=new Alumno();
+        
+        System.out.println("Tu edad es: " +a.getEdad()+" años");
+        System.out.println("Tu nombre es: "+a.getNombre());
+        System.out.println("Fuiste aprobado: "+a.isAprobado());
+        */
+        
+        JFrame v=new JFrame("   TITULO DE VENTANA"); //Para hacer ventanas
+            v.setSize(600, 400);
+            v.setVisible(true);
+    }
+    
+    
+}
